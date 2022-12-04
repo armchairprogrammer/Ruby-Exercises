cfb3c5f905baf46971e55e6574e7c844ceb0a744
armchairprogrammer <50150977+armchairprogrammer@users.noreply.github.com>
2022-12-04 17:59:52 +0100

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,7 +2,7 @@ def bubble_sort(num)
   loop do
     (num.size-1).times do |times| 
     if num[times] > num[times+1]
-      num[times], num[times+1] = num[times+1], num[times]
+      puts "Pass: #{times}" 
       puts num 
       end
     end
