9a44043faa2edcfe5a8e4afb6bb1a1d247955dfc
armchairprogrammer <50150977+armchairprogrammer@users.noreply.github.com>
2022-12-04 17:57:26 +0100

    Update bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,9 +1,10 @@
-loop do
-  (num.size-1).times do |i|
-    if num[i] > num[i+1]
-        num[i], num[i+1] = num[i+1], num[i]
-        
-        puts num
+def bubble_sort(num)
+  loop do
+    (num.size-1).times do |times| 
+    if num[times] > num[times+1]
+      num[times], num[times+1] = num[times+1], num[times]
+      puts num 
       end
     end
+  end
 end
